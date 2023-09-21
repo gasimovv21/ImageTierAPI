2eb355bedbcfcd115634e857f2debd719dec67e0
gasimovv21 <gasimoweltun@gmail.com>
2023-09-21 05:57:36 +0400

    Done: set up databse with models of user. id, name, surname, username, tier. TODO: Create images app, models, and set up ManyToManyField from user_account models to images.

--- ImageTierApi/user_accounts/admin.py
+++ ImageTierApi/user_accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserAccount
+
+
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'surname',
+        'username',
+        'tier',
+    )
+    search_fields = (
+        'id',
+        'name',
+        'surname',
+        'username',
+        'tier',
+    )
+    list_filter = (
+        'id',
+        'name',
+        'surname',
+        'username',
+        'tier',
+    )
+    empty_value_display = '-empty-'
--- ImageTierApi/user_accounts/models.py
+++ ImageTierApi/user_accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class UserAccount(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+
+    TIER_CHOICES = [
+        ('Basic', 'Basic'),
+        ('Premium', 'Premium'),
+        ('Enterprise', 'Enterprise'),
+    ]
+
+    tier = models.CharField(
+        max_length=20,
+        choices=TIER_CHOICES
+    )
